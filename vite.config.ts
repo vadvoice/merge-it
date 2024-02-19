165f7d73aba6723e3a290491d8e11aaeeb854a49
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2024-02-19 21:23:24 +0200

    chore: package config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import UnoCSS from 'unocss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
+  base: '/merge-it',
   build: {
     outDir: './docs'
   }
