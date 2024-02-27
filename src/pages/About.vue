a9c10f5806144ad6f9c3429d09f4f15d67df2beb
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2024-02-27 11:16:29 +0200

    feat: app favicon & better about page desc

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>About page</h1>
+  <div class="flex flex-col justify-between">
+    <div class="mx-10">
+      <h1 class="text-center flex flex-col items-center justify-center">
+        <img alt="Merge It!" src="../assets/logo-full.png" width="100" />
+        Merge it!
+      </h1>
+
+      <p class="text-center">
+        <strong>Merge It!</strong> is a game that allows you to build your own
+        avatar using a variety of options. You can change the face, hair, and
+        background color. You can also download the avatar you created.
+      </p>
+
+      <h2 class="text-center">How to play</h2>
+      <ul>
+        <li>
+          In the future, we plan to add more options for creating an avatar, as
+          well as the ability to share your avatar on social networks.
+        </li>
+        <li>
+          We hope you enjoy the game and have fun creating your own avatar!
+        </li>
+      </ul>
+    </div>
+
+    <footer>
+      <p class="text-center">
+        The game was created using Vue 3, Tailwind CSS, and Vite. It uses the
+        <a href="https://iconify.design/">Iconify</a> library to display the
+        icons.
+      </p>
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>
